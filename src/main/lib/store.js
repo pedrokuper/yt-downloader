d2f6a97263181265ed60e77a41df66db9692bd4b
Pedro Kupersmit <pkupersmit@gmail.com>
2024-11-02 21:24:57 -0300

    refactor: simplify data serialization in Store class & remove dead code

--- src/main/lib/store.js
+++ src/main/lib/store.js
@@ -16,11 +16,7 @@ export default class Store {
 	set(key, val) {
 		this.data[key] = val;
 		try {
-			// Serialize data with a replacer function to prevent circular references
-			fs.writeFileSync(
-				this.path,
-				JSON.stringify(this.data, getCircularReplacer())
-			);
+			fs.writeFileSync(this.path, JSON.stringify(this.data));
 		} catch (error) {
 			console.error("Error writing to store:", error);
 		}
@@ -28,26 +24,9 @@ export default class Store {
 }
 
 function parseDataFile(filePath, defaults) {
-	// We'll try/catch it in case the file doesn't exist yet, which will be the case on the first application run.
-	// `fs.readFileSync` will return a JSON string which we then parse into a Javascript object
 	try {
 		return JSON.parse(fs.readFileSync(filePath));
 	} catch (error) {
-		// if there was some kind of error, return the passed in defaults instead.
 		return defaults;
 	}
-}
-
-// Helper function to prevent circular reference errors
-function getCircularReplacer() {
-	const seen = new WeakSet();
-	return (key, value) => {
-		if (typeof value === "object" && value !== null) {
-			if (seen.has(value)) {
-				return; // Avoid circular reference
-			}
-			seen.add(value);
-		}
-		return value;
-	};
 }
